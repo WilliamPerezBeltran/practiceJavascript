b587d583a3d3cdc4e6f6ff282707005918f02837
WilliamPerezBeltran <williampbeltranprogramador@gmail.com>
2021-01-14 18:03:26 -0500

    add files javascript

--- map-filter-reduce/change_key_in_object.js
+++ map-filter-reduce/change_key_in_object.js
@@ -1,3 +1,32 @@
+// var projectArray = [[
+//   { _id: 6, total: 4478.4 },
+//   { _id: 1, total: 110248.13 },
+//   { _id: 7, total: 663695.1 }
+// ],
+// [
+//   { _id: 7, total: 31278 },
+//   { _id: 1, total: 67174.66 },
+//   { _id: 4, total: 3712.8 },
+//   { _id: 8, total: 670 }
+// ]];
+
+// var result = projectArray.flat().reduce( (acc,i) => {
+//    if(acc.hasOwnProperty(i._id))
+//        acc[i._id] += i.total;
+//    else
+//        acc[i._id] = i.total;
+//    return acc;
+// },{});
+// console.log(result);
+
+
+// {
+//   "1": 177422.79,
+//   "4": 3712.8,
+//   "6": 4478.4,
+//   "7": 694973.1,
+//   "8": 670
+// }
 let input = [{
         code_id: 4,
         dateTime: "1/9/2021 11:06:08 AM",
@@ -109,7 +138,6 @@ let mergeSum = sumInput.concat(sumOutput).reduce(function(r, o) {
 }, [])
 
 let resultBalance = mergeSum.map((item) => {
-    // return Object.assign(item,{balance:item.input-item.output})
     if (Object.keys(item).includes("output")) {
         return Object.assign(item, {
             balance: item.input - item.output

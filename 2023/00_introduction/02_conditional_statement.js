5a77ae113d329c8cbdeb2109afb7743bece68de7
WilliamPerezBeltran <williampbeltranprogramador@gmail.com>
2023-08-18 07:44:14 -0500

    add npm

---
+++ 2023/00_introduction/02_conditional_statement.js
@@ -0,0 +1,17 @@
+/* In javascript has a similir set of control structure (if(condition), if(condition)-else-end, if(condition)-else if (condition)-else-end)
+and (switch(condition)-case n -default-end)
+*/
+
+
+var x = true
+if(x){
+	console.log(x)
+}
+
+if(x){
+		console.log(x)
+else{
+	console.log("It is not x")
+}
+
+

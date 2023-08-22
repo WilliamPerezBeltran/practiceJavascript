830a49022d9e75bb4d73f38a8bef5f8539147266
WilliamPerezBeltran <williampbeltranprogramador@gmail.com>
2023-08-22 11:34:26 -0500

    add functions and objects

--- 2023/00_introduction/03_loops.js
+++ 2023/00_introduction/03_loops.js
@@ -1,18 +1,18 @@
-for (let i=0;i<10;i++){
-	console.log(i)
+for (let i = 0; i < 10; i++) {
+  console.log(i);
 }
 
 //==================
 
-let x =0  
-while(x <10){
-	console.log(x)
-	x++
+let x = 0;
+while (x < 10) {
+  console.log(x);
+  x++;
 }
 //==================
-let y = 0
+let y = 0;
 
-do{
-	console.log(y)
-	y++
-}while(y<10)
+do {
+  console.log(y);
+  y++;
+} while (y < 10);
